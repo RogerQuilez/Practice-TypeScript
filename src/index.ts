a95489cfa9dc1445644005ee782a8a775b749757
Roger Quilez Palleja <rogeerquilezpalleja@gmail.com>
2021-09-30 16:15:52 +0200

    Adding task with enter on input

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-let a = 'ewqeqweqweqweqw';
-console.log(a);
+/* ADDING TASK WITH ENTER ON INPUT */
+const taskList = document.querySelector('#taskList');
+const taskLeft = document.querySelector('#taskLeft');
+const input: HTMLInputElement = document.querySelector('#taskInput');
+const arrayLi: Array<HTMLElement> = [];
+input.addEventListener('keydown', (e: KeyboardEvent) => {
+
+    if (e.key == 'Enter') {
+        let newLi = document.createElement('li');
+        newLi.innerHTML = `<div>
+                                <input type="checkbox">
+                                <span>${input.value}</span>
+                            </div>
+                            <span class="material-icons btn-delete">delete_outline</span>`;
+        taskList.append(newLi);
+        arrayLi.push(newLi);
+        input.value = '';
+        changeTaskText();
+    }
+
+})
+
+function changeTaskText () {
+    taskLeft.innerHTML = `Quedan ${arrayLi.length} tareas`;
+}
