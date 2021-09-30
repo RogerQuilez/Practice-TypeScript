d5267bd5a860aadc6a5b819d40ee82c054aa23fd
Roger Quilez Palleja <rogeerquilezpalleja@gmail.com>
2021-09-30 16:53:39 +0200

    Adding deleting function

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,64 @@
-/* ADDING TASK WITH ENTER ON INPUT */
+class List {
+
+    public name: string;
+    public done: boolean;
+
+    constructor(name: string) {
+        this.name = name;
+        this.done = false;
+    }
+}
+
 const taskList = document.querySelector('#taskList');
 const taskLeft = document.querySelector('#taskLeft');
 const input: HTMLInputElement = document.querySelector('#taskInput');
-const arrayLi: Array<HTMLElement> = [];
+const arrayLi: Array<List> = [];
+const deleteButton: any = document.querySelectorAll('.btn-delete');
+
+/* ADDING TASK WITH ENTER ON INPUT */
 input.addEventListener('keydown', (e: KeyboardEvent) => {
 
     if (e.key == 'Enter') {
+
+        if (!input.value) {
+            return;
+        }
+
         let newLi = document.createElement('li');
-        newLi.innerHTML = `<div>
-                                <input type="checkbox">
-                                <span>${input.value}</span>
-                            </div>
-                            <span class="material-icons btn-delete">delete_outline</span>`;
+        let newList = new List(input.value);
+
+        printTask(newLi, newList);
+
         taskList.append(newLi);
-        arrayLi.push(newLi);
+        arrayLi.push(newList);
+
+        let deleteButtons: any = document.querySelectorAll('.btn-delete');
+        console.log(deleteButtons);
+        addDeleteButtonFunction(deleteButtons);
+        
         input.value = '';
         changeTaskText();
     }
 
 })
 
-function changeTaskText () {
+function printTask(newLi: HTMLElement, newList: List) {
+    newLi.innerHTML = `<div>
+                            <input type="checkbox">
+                            <span>${newList.name}</span>
+                            </div>
+                            <span class="material-icons btn-delete">delete_outline</span>`;
+}
+
+function changeTaskText() {
     taskLeft.innerHTML = `Quedan ${arrayLi.length} tareas`;
+}
+
+/* Adding deleting function */
+function addDeleteButtonFunction(deleteButtons: Array<HTMLElement>) {
+    for (let deleteButton of deleteButtons) {
+        deleteButton.addEventListener('click', () => {
+            deleteButton.parentElement.remove();
+        })
+    }
 }
